2ca6c54e8e41a35b5532c7f121e74f3ef3aac383
jodli <job87@web.de>
2018-01-16 15:20:28 +0100

    Added booking positions to array first. Minor fixes in typing / waiting for input fields.

--- index.js
+++ index.js
@@ -72,12 +72,12 @@ async function startAndNavigateToProjectTimes(options) {
 	page.goto(process.env.BASE_URL, {
 		waitUntil: 'domcontentloaded'
 	});
-	await page.waitFor(5000);
+	await page.waitFor(10000);
 
-	const responseAuth = await page.authenticate({
-		username: process.env.AUTH_USER,
-		password: process.env.AUTH_PASS
-	});
+	//const responseAuth = await page.authenticate({
+	//	username: process.env.AUTH_USER,
+	//	password: process.env.AUTH_PASS
+	//});
 
 	log.info('Navigate to project times');
 	await page.goto(process.env.BASE_URL + process.env.PROJECTS_URL, {
@@ -99,7 +99,7 @@ async function getProjects(outFile, options) {
 		separator: ';'
 	});
 	writer.pipe(fs.createWriteStream(outFile));
-	
+
 	const { page, browser } = await startAndNavigateToProjectTimes(options);
 
 	log.info('Getting available projects');
@@ -133,7 +133,7 @@ async function getProjects(outFile, options) {
 		log.info(registrations);
 
 		log.info('Writing data to csv');
-		writer.write({Project: projects[i], Registrations: registrations});
+		writer.write({ Project: projects[i], Registrations: registrations });
 	}
 
 	log.info('Done.');
@@ -146,6 +146,21 @@ async function getProjects(outFile, options) {
 async function bookProjects(inFile, options) {
 	const csvReader = require('fast-csv');
 
+	let bookingPositions = [];
+	log.info('Setting up csv reader.');
+	const csvStream = csvReader.fromStream(
+		fs.createReadStream(inFile), {
+			delimiter: ';',
+			headers: true
+		}
+	).on('data', async (data) => {
+		log.info(data);
+		bookingPositions.push(data);
+		log.info("Booking position list now contains: " + bookingPositions.length);
+	}).on('end', async (data) => {
+		log.info('No more rows.');
+	});
+
 	const projektInputSelector = '#ctl00_cphContent_cboProjects_I';
 	const projektErfassungSelector = '#ctl00_cphContent_cboPSItem_0_I';
 	const datumSelector = '#ctl00_cphContent_ctl37_I';
@@ -154,73 +169,64 @@ async function bookProjects(inFile, options) {
 	const projektzeitErfassenSelector = '#ctl00_cphContent_cmdTimeRegAmount_CD';
 
 	const { page, browser } = await startAndNavigateToProjectTimes(options);
-	
+
 	await page.waitFor(5000);
 
-	log.info('Setting up csv reader.');
-	const csvStream = csvReader.fromStream(
-		fs.createReadStream(inFile), {
-			delimiter: ';',
-			headers: true
-		}
-	)
-	.on('data', async (data) => {
-		log.info(data);
+	for (let i = 0; i < bookingPositions.length; i++) {
+		const element = bookingPositions[i];
 		await page.waitFor(2000);
-		
-		log.info('Setting project to', data.Project);
+
+		log.info('Setting project to', element.Project);
 		const projectInput = await focusInputField(page, projektInputSelector);
 		await selectAllText(page);
-		typeInInputField(projectInput, data.Project);
+		typeInInputField(projectInput, element.Project);
 		await waitForLoadingElements(page);
 		await page.waitFor(2000);
 		await selectFirstEntryInDropDown(projectInput);
 		await waitForNavigation(page);
-		
-		log.info('Setting registration to', data.Registration);
+
+		log.info('Setting registration to', element.Registration);
 		const registrationInput = await focusInputField(page, projektErfassungSelector);
 		await selectAllText(page);
-		typeInInputField(registrationInput, data.Project);
+		typeInInputField(registrationInput, element.Registration);
 		await page.waitFor(2000);
 		await selectFirstEntryInDropDown(registrationInput);
 		await waitForNavigation(page);
-		
-		log.info('Setting date to', data.Date);
+
+		log.info('Setting date to', element.Date);
 		const dateInput = await focusInputField(page, datumSelector);
 		await selectAllText(page);
-		typeInInputField(dateInput, data.Date);
+		typeInInputField(dateInput, element.Date);
 		await page.waitFor(2000);
 		await selectFirstEntryInDropDown(dateInput);
 		await waitForNavigation(page);
-		
-		log.info('Setting duration to', data.Duration);
+
+		log.info('Setting duration to', element.Duration);
 		const durationInput = await focusInputField(page, dauerSelector);
-		typeInInputField(durationInput, data.Duration);
+		await selectAllText(page);
+		typeInInputField(durationInput, element.Duration);
 		await page.waitFor(2000);
-		
-		log.info('Setting comment to', data.Comment);
+
+		log.info('Setting comment to', element.Comment);
 		const commentInput = await focusInputField(page, bemerkungenSelector);
 		await selectAllText(page);
-		typeInInputField(commentInput, data.Comment);
+		typeInInputField(commentInput, element.Comment);
 		await page.waitFor(2000);
-		
+
 		log.info('Submitting booking.');
 		await page.click(projektzeitErfassenSelector);
 		await waitForNavigation(page);
-		
-		log.info('Successfully booked entry:', data);
-	})
-	.on('end', async (data) => {
-		log.info('No more rows.');
-		
-		log.info('Done.');
-		//browser.close();
-		log.info('Closed browser.');
-	});
+
+		log.info('Successfully booked entry:', element);
+	}
+
+	log.info('Done.');
+	//browser.close();
+	log.info('Closed browser.');
 }
 
 log.setLevel('warn');
-program	.version('0.0.1');
+program.version('0.1.0');
 
 program
 	.command('getProjects <path>')
@@ -247,7 +253,7 @@ program
 		log.info('Getting projects and their registrations and writing them to: ' + path);
 		getProjects(path, options);
 	});
-	
+
 program
 	.command('bookProjects <path>')
 	.description('Books all positions contained in the .csv file at the given [path].')
